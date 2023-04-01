30a32f563c0c949ed38f113e3c315fcabdc5174f
Ausianovich Kanstantsin <ausianovich.kanstantsin@gmail.com>
2023-04-01 20:18:44 +0300

    Add two library

---
+++ Sources/RootLibrary/RootLibrary.swift
@@ -0,0 +1,6 @@
+public struct RootLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/RootLibraryTests/RootLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RootLibrary
+
+final class RootLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RootLibrary().text, "Hello, World!")
+    }
+}
